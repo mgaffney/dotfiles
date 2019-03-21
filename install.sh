ecf4742561317e469a47147ddd15a65c311b0b8e
Michael Gaffney <mike@gaffney.cc>
2019-03-21 10:31:50 -0400

    Install alacritty

--- install.sh
+++ install.sh
@@ -2,56 +2,47 @@
 
 # The following options are the equivalent of set -eux
 # exit the script if we run into errors (-e)
-set -o errexit
+#set -o errexit
 # accessing an unset variable or parameter should cause an error (-u)
-set -o nounset
+#set -o nounset
 # print a trace of commands (-x)
 # set -x xtrace
 
-#  Trap non-normal exit signals: 1/HUP, 2/INT, 3/QUIT, 15/TERM, ERR
-trap founderror 1 2 3 15 ERR
-
-founderror() {
-  exit 1
-}
-
-exitscript() {
-  exit 0
-}
-
-##
-# warn: Print a message to stderr.
-# Usage: warn "message"
-#
-warn() {
-  printf 'warning: %s\n' "$@" >&2
-}
-
-##
-# die: Print a message to stderr and exit with 
-# status code 1
-# Usage: some_command || die "message" 
-#        some_command && die "message" 
-#
-die() {
-  warn "$1"
-  founderror
-} 
-
-install_git() {
-  local readonly target=~/.config/git
-  [[ -e $target ]] && die "$target already exists" 
-  mkdir -p ~/.config
-  ln -nsf "$srcdir/git" $target
-}
-
-install_tmux() {
-  ln -nsf "$srcdir/tmux/tmux.conf" $HOME/.tmux.conf
-}
-
 declare -r srcdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-install_tmux
-install_git
+function err() {
+  echo "$@" >&2
+}
 
-exitscript
+function install_git() {
+  local readonly target="${HOME}/.config/git"
+  if [[ -d "${target}" ]]; then
+    mv "${target}" "${HOME}/.config/git-orig"
+  fi
+  ln -nsf "${srcdir}/git" "${target}"
+}
+
+function install_tmux() {
+  local readonly target="${HOME}/.tmux.conf"
+  if [[ -s "${target}" ]]; then
+    mv "${target}" "${HOME}/.tmux.conf.orig"
+  fi
+  ln -nsf "${srcdir}/tmux/tmux.conf" "${target}"
+}
+
+function install_alacritty() {
+  local readonly target="${HOME}/.config/alacritty"
+  if [[ -d "${target}" ]]; then
+    mv "${target}" "${HOME}/.config/alacritty-orig"
+  fi
+  ln -nsf "${srcdir}/alacritty" "${target}"
+}
+
+function main() {
+  mkdir -p "${HOME}/.config"
+  install_tmux
+  install_git
+  install_alacritty
+}
+
+main "$@"
