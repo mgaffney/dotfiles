4e916769e0eae330eee6d3b8f1658d196cc4d704
Michael Gaffney <mike@gaffney.cc>
2019-03-22 07:51:22 -0400

    Install zsh files

--- install.sh
+++ install.sh
@@ -8,41 +8,77 @@
 # print a trace of commands (-x)
 # set -x xtrace
 
-declare -r srcdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+readonly SCRIPTDIR
 
 function err() {
   echo "$@" >&2
 }
 
-function install_git() {
-  local readonly target="${HOME}/.config/git"
-  if [[ -d "${target}" ]]; then
-    mv "${target}" "${HOME}/.config/git-orig"
+function backup_dir() {
+  local -r src="$1"
+  local -r target="${src}-orig"
+
+  if [[ -d "${src}" ]]; then
+    if [[ -d "${target}" ]]; then
+      err "Error: ${target} directory already exists"
+      return 1
+    fi
+    mv "${src}" "${target}"
   fi
-  ln -nsf "${srcdir}/git" "${target}"
+}
+
+function backup_file() {
+  local -r src="$1"
+  local -r target="${src}-orig"
+
+  if [[ -f "${src}" ]]; then
+    if [[ -f "${target}" ]]; then
+      err "Error: ${target} file already exists"
+      return 1
+    fi
+    mv "${src}" "${target}"
+  fi
+}
+
+function backup() {
+  local -r src="$1"
+  if [[ -d "${src}" ]]; then
+    backup_dir "${src}"
+  elif [[ -f "${src}" ]]; then
+    backup_file "${src}"
+  fi
+}
+
+function install() {
+  local -r src="$1"
+  local -r target="$2"
+  backup "${target}" || return
+  ln -nsf "${src}" "${target}"
 }
 
 function install_tmux() {
-  local readonly target="${HOME}/.tmux.conf"
-  if [[ -s "${target}" ]]; then
-    mv "${target}" "${HOME}/.tmux.conf.orig"
-  fi
-  ln -nsf "${srcdir}/tmux/tmux.conf" "${target}"
+  install "${SCRIPTDIR}/tmux/tmux.conf" "${HOME}/.tmux.conf"
+}
+
+function install_git() {
+  install "${SCRIPTDIR}/git" "${HOME}/.config/git"
 }
 
 function install_alacritty() {
-  local readonly target="${HOME}/.config/alacritty"
-  if [[ -d "${target}" ]]; then
-    mv "${target}" "${HOME}/.config/alacritty-orig"
-  fi
-  ln -nsf "${srcdir}/alacritty" "${target}"
+  install "${SCRIPTDIR}/alacritty" "${HOME}/.config/alacritty"
+}
+
+function install_zsh() {
+  install "${SCRIPTDIR}/zsh/zshrc" "${HOME}/.zshrc"
 }
 
 function main() {
   mkdir -p "${HOME}/.config"
-  install_tmux
-  install_git
-  install_alacritty
+  install_tmux || return
+  install_git || return
+  install_alacritty || return
+  install_zsh || return
 }
 
 main "$@"
