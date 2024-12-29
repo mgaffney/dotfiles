642e6fd60ca679d94b06283536c2217e4817d3cc
Michael Gaffney <mike@gaffney.cc>
2024-12-29 09:40:22 -0500

    neovim: Enable nerd font diagnostic glyphs

--- nvim/init.lua
+++ nvim/init.lua
@@ -681,16 +681,16 @@ endif
       })
 
       -- Change diagnostic symbols in the sign column (gutter)
-      -- if vim.g.have_nerd_font then
-      --   local signs = { ERROR = '', WARN = '', HINT = '', INFO = '' }
-      --   local diagnostic_signs = {}
-      --   for type, icon in pairs(signs) do
-      --     local hl = 'DiagnosticSign' .. type
-      --     diagnostic_signs[vim.diagnostic.severity[type]] = icon
-      --     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-      --   end
-      --   vim.diagnostic.config { signs = { text = diagnostic_signs } }
-      -- end
+      if vim.g.have_nerd_font then
+        local signs = { ERROR = '', WARN = '', HINT = '', INFO = '' }
+        local diagnostic_signs = {}
+        for type, icon in pairs(signs) do
+          local hl = 'DiagnosticSign' .. type
+          diagnostic_signs[vim.diagnostic.severity[type]] = icon
+          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+        vim.diagnostic.config { signs = { text = diagnostic_signs } }
+      end
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
