99200dafc2e7159b11b96d78004202bfa5622792
Michael Gaffney <mike@gaffney.cc>
2025-01-10 09:18:53 -0500

    neovim: Add obsidian plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -852,6 +852,27 @@ endif
     },
   },
 
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+      "nvim-telescope/telescope.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "Notes",
+          path = "/Users/mgaffney/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notes",
+        },
+      },
+    },
+  },
+
   { -- Autocompletion
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
