2be8d82cc0960d0e4faaba9b0f077cff58ef6f91
Michael Gaffney <mike@gaffney.cc>
2025-01-24 11:22:18 -0500

    neovim(obsidian): Refactor obsidian config into plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -855,73 +855,6 @@ endif
     },
   },
 
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-      "nvim-telescope/telescope.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "Notes",
-          path = "/Users/mgaffney/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notes",
-        },
-      },
-
-      --[[
-Lua Date and Time format tags
-https://www.lua.org/pil/22.1.html
-
-%a	abbreviated weekday name (e.g., Wed)
-%A	full weekday name (e.g., Wednesday)
-%b	abbreviated month name (e.g., Sep)
-%B	full month name (e.g., September)
-%c	date and time (e.g., 09/16/98 23:48:10)
-%d	day of the month (16) [01-31]
-%H	hour, using a 24-hour clock (23) [00-23]
-%I	hour, using a 12-hour clock (11) [01-12]
-%M	minute (48) [00-59]
-%m	month (09) [01-12]
-%p	either "am" or "pm" (pm)
-%S	second (10) [00-61]
-%w	weekday (3) [0-6 = Sunday-Saturday]
-%x	date (e.g., 09/16/98)
-%X	time (e.g., 23:48:10)
-%Y	full year (1998)
-%y	two-digit year (98) [00-99]
-%%	the character `%´
---]]
-
-      templates = {
-        folder = "templates",
-        date_format = "%Y-%m-%d",
-        time_format = "%H:%M",
-        -- A map for custom variables, the key should be the variable and the value a function
-        substitutions = {},
-      },
-
-      daily_notes = {
-        -- Optional, if you keep daily notes in a separate directory.
-        folder = "daily",
-        -- Optional, if you want to change the date format for the ID of daily notes.
-        date_format = "%Y-%m-%d",
-        -- Optional, if you want to change the date format of the default alias of daily notes.
-        alias_format = "%A %B %-d, %Y",
-        -- Optional, default tags to add to each new daily note created.
-        default_tags = { "daily-notes" },
-        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-        template = "daily.md"
-      },
-
-    },
-  },
-
   { -- Autocompletion
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
@@ -1258,7 +1191,7 @@ set grepprg=internal
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,69 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = false,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+		"nvim-telescope/telescope.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "Notes",
+				path = "/Users/mgaffney/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notes",
+			},
+		},
+
+		templates = {
+			folder = "templates",
+			date_format = "%Y-%m-%d",
+			time_format = "%H:%M",
+			-- A map for custom variables, the key should be the variable and the value a function
+			substitutions = {},
+		},
+
+		daily_notes = {
+			-- Optional, if you keep daily notes in a separate directory.
+			folder = "daily",
+			-- Optional, if you want to change the date format for the ID of daily notes.
+			date_format = "%Y-%m-%d",
+			-- Optional, if you want to change the date format of the default alias of daily notes.
+			alias_format = "%A %B %-d, %Y",
+			-- Optional, default tags to add to each new daily note created.
+			default_tags = { "daily-notes" },
+			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+			template = "daily.md"
+		},
+
+	},
+}
+
+--[[
+Lua Date and Time format tags
+https://www.lua.org/pil/22.1.html
+
+%a	abbreviated weekday name (e.g., Wed)
+%A	full weekday name (e.g., Wednesday)
+%b	abbreviated month name (e.g., Sep)
+%B	full month name (e.g., September)
+%c	date and time (e.g., 09/16/98 23:48:10)
+%d	day of the month (16) [01-31]
+%H	hour, using a 24-hour clock (23) [00-23]
+%I	hour, using a 12-hour clock (11) [01-12]
+%M	minute (48) [00-59]
+%m	month (09) [01-12]
+%p	either "am" or "pm" (pm)
+%S	second (10) [00-61]
+%w	weekday (3) [0-6 = Sunday-Saturday]
+%x	date (e.g., 09/16/98)
+%X	time (e.g., 23:48:10)
+%Y	full year (1998)
+%y	two-digit year (98) [00-99]
+%%	the character `%´
+--]]
