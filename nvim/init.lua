7ed95aa6199f705a71956bf0decb87ff0c8c848c
Michael Gaffney <mike@gaffney.cc>
2025-04-22 15:58:05 -0400

    neovim(blink): disable blink on command line
    Work around for bug when using vim-grepper.
    
    See https://github.com/Saghen/blink.cmp/issues/1512

--- nvim/init.lua
+++ nvim/init.lua
@@ -955,6 +955,8 @@ endif
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
       },
+      -- Disable cmdline
+      cmdline = { enabled = false },
 
       sources = {
         default = { 'lsp', 'path', 'snippets', 'lazydev' },
