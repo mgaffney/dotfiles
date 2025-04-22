c70e726719da6d3efbdf7e2b0f37fa7c3d4d27bb
Michael Gaffney <mike@gaffney.cc>
2025-04-22 15:57:15 -0400

    neovim: configure blink to use rust version

--- nvim/init.lua
+++ nvim/init.lua
@@ -972,11 +972,12 @@ endif
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
     },
+    opts_extend = { "sources.default" }
   },
 
   {
